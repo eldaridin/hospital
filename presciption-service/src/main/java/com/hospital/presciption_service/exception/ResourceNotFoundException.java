32d4b400e1ec1680a12461837be5d541c5eca8a0
María José Varga G. <mariajosevarga@gmail.com>
2026-06-17 14:35:14 -0400

    refactor: elimina modulo duplicado presciption-service y preserva la nomenclatura correcta

--- presciption-service/src/main/java/com/hospital/presciption_service/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.hospital.presciption_service.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
