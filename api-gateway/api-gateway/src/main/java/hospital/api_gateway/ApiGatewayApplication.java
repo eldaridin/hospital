c6de639e5b131afbb1d9004e8b9b79202441c570
Dario <dario.rojas.monsalvez@gmail.com>
2026-06-22 19:08:15 -0400

    MATENME :)
    :)

--- api-gateway/api-gateway/src/main/java/hospital/api_gateway/ApiGatewayApplication.java
+++ api-gateway/api-gateway/src/main/java/hospital/api_gateway/ApiGatewayApplication.java
@@ -2,8 +2,12 @@ package hospital.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
