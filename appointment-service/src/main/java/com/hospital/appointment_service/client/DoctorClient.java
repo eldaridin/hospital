f54e326a93faa0d7cbbf68e941409a47acca3225
Dario <dario.rojas.monsalvez@gmail.com>
2026-06-23 12:02:17 -0400

    Arreglos Random

--- appointment-service/src/main/java/com/hospital/appointment_service/client/DoctorClient.java
+++ appointment-service/src/main/java/com/hospital/appointment_service/client/DoctorClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "doctor-service", url = "http://localhost:8084")
+@FeignClient(name = "doctor-service")
 public interface DoctorClient {
 
     @GetMapping("/api/doctores/{id}")
