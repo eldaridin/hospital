68f813e4732a9fd7af0644a0e48a360ae63610fc
Dario <dario.rojas.monsalvez@gmail.com>
2026-06-25 13:19:43 -0400

    Dockerfiles y docker-compose actualizado-v1.0

--- appointment-service/src/main/java/com/hospital/appointment_service/AppointmentServiceApplication.java
+++ appointment-service/src/main/java/com/hospital/appointment_service/AppointmentServiceApplication.java
@@ -3,7 +3,7 @@ package com.hospital.appointment_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
 public class AppointmentServiceApplication {
--- inventory-service/src/main/java/hospital/inventory_service/controller/HerramientaController.java
+++ inventory-service/src/main/java/hospital/inventory_service/controller/HerramientaController.java
@@ -3,6 +3,7 @@ package hospital.inventory_service.controller;
 import hospital.inventory_service.model.Herramienta;
 import hospital.inventory_service.service.HerramientaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,10 +19,15 @@ public class HerramientaController {
         return herramientaService.findAll();
     }
 
-    @GetMapping("/id")
-    public Herramienta findById(Integer id) {
-        return herramientaService.findById(id);
+    @GetMapping("/{id}")
+    public ResponseEntity<Herramienta> findById(@PathVariable Integer id) {
+        Herramienta herramienta = herramientaService.findById(id);
+        if (herramienta == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(herramienta);
     }
+
 
     @PostMapping
     public Herramienta save(@RequestBody Herramienta herramienta) {
@@ -29,8 +35,9 @@ public class HerramientaController {
     }
 
     @PutMapping("/{id}")
-    public Herramienta update(@PathVariable Integer id, @RequestBody Herramienta herramienta) {
-        return herramientaService.save(herramienta);
+    public ResponseEntity<Herramienta> update(@PathVariable Integer id, @RequestBody Herramienta herramienta) {
+        herramienta.setId(id);
+        return ResponseEntity.ok(herramientaService.save(herramienta));
     }
 
     @DeleteMapping("/{id}")
