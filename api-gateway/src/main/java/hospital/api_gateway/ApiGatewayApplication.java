1b793eabaaf05851915efee0764de8060ffd2842
Dario <dario.rojas.monsalvez@gmail.com>
2026-06-25 17:08:43 -0400

    Intentando solucionar api-gateway

--- api-gateway/src/main/java/hospital/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/hospital/api_gateway/ApiGatewayApplication.java
@@ -1,14 +1,13 @@
 package hospital.api_gateway;
 
-import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-@EnableDiscoveryClient
+
 @SpringBootApplication
 public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
-
 }
+
+
