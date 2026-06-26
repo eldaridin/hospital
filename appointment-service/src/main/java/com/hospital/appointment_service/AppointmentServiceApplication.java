23bc85bcdcb5f713f65e7dd6fb4e0a3627031a37
Angel Gonzalez <Gonzalezdugarte2005@gmail.com>
2026-06-26 14:12:34 -0400

    XD

--- appointment-service/src/main/java/com/hospital/appointment_service/AppointmentServiceApplication.java
+++ appointment-service/src/main/java/com/hospital/appointment_service/AppointmentServiceApplication.java
@@ -3,6 +3,7 @@ package com.hospital.appointment_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
